3dd6122f15e435a7fd08bbe546f2f4eb18aa6b0a
unknown <ausbin@PC88629.ihelse.net>
2015-12-14 13:29:37 +0100

    Cleanup.

--- p14.py
+++ p14.py
@@ -2,47 +2,38 @@ import functools
 import itertools
 
 REFLECTIONS = {
-    0: 0,
-    1: 1,
-    6: 9,
-    8: 8,
-    9: 6
+    '0': '0',
+    '1': '1',
+    '6': '9',
+    '8': '8',
+    '9': '6'
 }
 
 IDENTITY_REFLECTIONS = [k for k, v in REFLECTIONS.items() if k == v]
 
 
 def mirrorables(size):
-    if size < 1:
-        raise ValueError("size argument must be positive.")
-
-    yield from filter(
-        lambda x: x[0] != 0 and x[-1] != 0,
-        itertools.product(*([REFLECTIONS.keys()] * size)))
+    products = itertools.product(*([REFLECTIONS.keys()] * size))
+    return (''.join(s) for s in products)
 
 
 def make_mirror(s):
-    return tuple(reversed([REFLECTIONS[c] for c in s]))
-
-
-def _mirrored():
-    yield from ((ir,) for ir in IDENTITY_REFLECTIONS)
-
-    for size in itertools.count(1):
-        for m in mirrorables(size):
-            yield m + make_mirror(m)
-
-        for m in mirrorables(size):
-            for mid in IDENTITY_REFLECTIONS:
-                yield m + (mid,) + make_mirror(m)
-
-
-def to_int(s):
-    return functools.reduce(
-        lambda r, c: r * 10 + c, s, 0)
+    return ''.join(reversed([REFLECTIONS[c] for c in s]))
 
 
 def mirrored():
-    return map(to_int, _mirrored())
+    yield from (ir for ir in IDENTITY_REFLECTIONS)
 
+    for size in itertools.count(1):
+        yield from (
+            m + make_mirror(m)
+            for m in mirrorables(size)
+            if m[0] != '0'
+        )
 
+        yield from (
+            m + mid + make_mirror(m)
+            for m in mirrorables(size)
+            for mid in IDENTITY_REFLECTIONS
+            if m[0] != '0'
+        )
--- p14_tests.py
+++ p14_tests.py
@@ -3,33 +3,29 @@ import unittest
 from p14 import *
 
 
-def _is_mirrored(a):
-    s = str(a)
+def _is_mirrored(s):
     for i in range(len(s) // 2):
-        front = int(s[i])
-        back = int(s[(i + 1) * -1])
+        front = s[i]
+        back = s[(i + 1) * -1]
         if REFLECTIONS[front] != back:
             return False
     return True
 
 
-class TestsToInt(unittest.TestCase):
-    def test_empty(self):
-        self.assertEqual(to_int(()), 0)
-
-    def test_simple(self):
-        self .assertEqual(to_int((1, 2, 3)), 123)
-        self.assertEqual(to_int((4, 5, 6)), 456)
-
-    def test_leading_zero(self):
-        self.assertEqual(to_int((0, 1, 2, 3)), 123)
-
-
 class TestMirrorables(unittest.TestCase):
+    def test_length_1(self):
+        self.assertEqual(
+            list(sorted(map(int, mirrorables(1)))),
+            [0, 1, 6, 8, 9])
+
     def test_length_2(self):
         self.assertEqual(
-            list(sorted(map(to_int, mirrorables(2)))),
-            [11, 16, 18, 19, 61, 66, 68, 69, 81, 86, 88, 89, 91, 96, 98, 99]
+            list(sorted(map(int, mirrorables(2)))),
+            [0, 1, 6, 8, 9,
+             10, 11, 16, 18, 19,
+             60, 61, 66, 68, 69,
+             80, 81, 86, 88, 89,
+             90, 91, 96, 98, 99]
         )
 
 
